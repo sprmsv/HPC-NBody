d988c9b4b1544dbf6a9fc3b5d9d8aca06376aa1a
Sepehr Mousavi <sepehrmousavi@izar1.izar.cluster>
2023-12-31 17:48:01 +0100

    local copy of particle

--- src/cuda/bruteforce.cu
+++ src/cuda/bruteforce.cu
@@ -7,7 +7,6 @@ __host__ void nbodybruteforce(particle_t* array, int nbr_particles, int nbr_iter
 	// Assign grid and block sizes
 	dim3 bsz(threads_per_block);
 	dim3 gsz(nbr_particles / threads_per_block + 1);
-	const int threads_per_block_rem = nbr_particles % threads_per_block;
 
 	// Launch the kernels in each iteration and synchronize
 	for (int it = 0; it < nbr_iterations; it++){
@@ -24,16 +23,20 @@ __host__ void nbodybruteforce(particle_t* array, int nbr_particles, int nbr_iter
 __global__ void compute_brute_force(particle_t* array, int nbr_particles)
 {
 	// Return if the thread is extra (can only happen in the last block)
-	int idx = offset + blockIdx.x * blockDim.x + threadIdx.x;
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if (idx >= nbr_particles) {
 		return;
 	}
 	
-	// TODO: Copy or reference or pointer?
-	// Get the thread particle
+	// Get the thread particle and copy its attributes
 	particle_t* p = &array[idx];
+	const double p_m = p->m;
+	const int p_id = p->id;
+	const double p_x = p->x[0];
+	const double p_y = p->x[1];
+	const double p_z = p->x[2];
 
-	// Declare variables
+	// Declare other local variables
 	double x_sep, y_sep, z_sep, dist_sq, grav_base;
 	double F_x = 0.;
 	double F_y = 0.;
@@ -44,23 +47,23 @@ __global__ void compute_brute_force(particle_t* array, int nbr_particles)
 
 	// Loop over source particles and accumulate local forces
 	for (int i = 0; i < nbr_particles; i++) {
-		if (array[i].id == p->id) {
+		if (array[i].id == p_id) {
 			continue;
 		}
-		x_sep = array[i].x[0] - p->x[0];
-		y_sep = array[i].x[1] - p->x[1];
-		z_sep = array[i].x[2] - p->x[2];
+		x_sep = array[i].x[0] - p_x;
+		y_sep = array[i].x[1] - p_y;
+		z_sep = array[i].x[2] - p_z;
 		dist_sq = max((x_sep * x_sep) + (y_sep * y_sep) + (z_sep * z_sep), 0.01);
-		grav_base = GRAV_CONSTANT * (p->m) * (array[i].m) / dist_sq / sqrt(dist_sq);
+		grav_base = GRAV_CONSTANT * (p_m) * (array[i].m) / dist_sq / sqrt(dist_sq);
 		F_x += grav_base * x_sep;
 		F_y += grav_base * y_sep;
 		F_z += grav_base * z_sep;
 	}
 
 	// Compute accelerations locally 
-	a_x = F_x / p->m;
-	a_y = F_y / p->m;
-	a_z = F_z / p->m;
+	a_x = F_x / p_m;
+	a_y = F_y / p_m;
+	a_z = F_z / p_m;
 
 	// Increment particle velocities
 	p->v[0] += a_x * TIMESTEP;
@@ -72,12 +75,11 @@ __global__ void compute_brute_force(particle_t* array, int nbr_particles)
 __global__ void update_positions(particle_t* array, int nbr_particles)
 {
 	// Return if the thread is extra (can only happen in the last block)
-	int idx = offset + blockIdx.x * blockDim.x + threadIdx.x;
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if (idx >= nbr_particles) {
 		return;
 	}
 
-	// TODO: Copy or reference or pointer?
 	// Get the thread particle
 	particle_t* p = &array[idx];
 
